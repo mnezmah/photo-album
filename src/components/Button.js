96c7a1ff0b6864b2ffb609f58453131cbfd3a0c0
mnezmah <mario.nezmah@gmail.com>
2019-06-18 13:34:54 +0200

    created button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types' 
 
 export default class Button extends Component {
+  static propTypes = {
+    onclick: PropTypes.func.isRequired,
+  }
   render() {
     return (
     <button>
-
+      click me
     </button>
     )
   }
